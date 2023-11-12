950e1ffb19a60fe842f0ba1d3820dcde63b38382
nguyenkhackien <20020679@vnu.edu.vn>
2023-11-12 23:15:25 +0700

    DCT

--- assets/src/DCT.py
+++ assets/src/DCT.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 from PIL import Image
 
-
 def dct(image):
     height, width = image.shape
     block_size = 8
@@ -13,30 +12,31 @@ def dct(image):
     new_image[:height, :width] = image
 
     new_image = new_image.astype(float)
-    new_image = new_image - 128
+    new_image -= 128
+
     result = np.zeros_like(new_image)
-    # duyet ma tran anh
+
     for i in range(0, blocks_h, block_size):
         for j in range(0, blocks_w, block_size):
-            # duyet tung khoi 8x8
             for u in range(i, i + block_size):
                 for v in range(j, j + block_size):
+                    cu = 1 / np.sqrt(2) if u % 8 == 0 else 1
+                    cv = 1 / np.sqrt(2) if v % 8 == 0 else 1
                     sum = 0
                     for x in range(block_size):
                         for y in range(block_size):
                             sum += new_image[x + i, y + j] * np.cos((2 * x + 1) * (u % 8) * np.pi / 16) * np.cos(
                                 (2 * y + 1) * (v % 8) * np.pi / 16)
-                    sum *= 1 / np.sqrt(2) if u % 8 == 0 else 1
-                    sum *= 1 / np.sqrt(2) if v % 8 == 0 else 1
-                    result[u, v] = sum * (1 / 4)
-    np.round(result)
-    return result
+                    result[u, v] = sum * cu * cv * (1 / 4)
 
+    return result
 
 def idct(result):
     height, width = result.shape
     block_size = 8
+
     image = np.zeros((height, width))
+
     for i in range(0, height, block_size):
         for j in range(0, width, block_size):
             for x in range(i, i + block_size):
@@ -44,10 +44,10 @@ def idct(result):
                     sum = 0
                     for u in range(block_size):
                         for v in range(block_size):
-                            sum += result[u + i, v + j] * np.cos((2 * (x % 8) + 1) * u * np.pi / 16) * np.cos(
+                            cu = 1 / np.sqrt(2) if u == 0 else 1
+                            cv = 1 / np.sqrt(2) if v == 0 else 1
+                            sum += result[u + i, v + j] * cu * cv * np.cos((2 * (x % 8) + 1) * u * np.pi / 16) * np.cos(
                                 (2 * (y % 8) + 1) * v * np.pi / 16)
-                            sum *= 1 / np.sqrt(2) if u == 0 else 1
-                            sum *= 1 / np.sqrt(2) if v == 0 else 1
                     image[x, y] = sum * (1 / 4) + 128
-    np.round(image)
+
     return image
--- assets/src/main.py
+++ assets/src/main.py
@@ -4,10 +4,6 @@ import matplotlib.pyplot as plt
 image = cv2.imread('../image/lena.jpg', cv2.IMREAD_GRAYSCALE)
 result = dct(image)
 reconstruction = idct(result)
-# cv2.imshow('original',image)
-# cv2.imshow('reconstruction',reconstruction)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
 
 
 plt.gray()
