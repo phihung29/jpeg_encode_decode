b6ebe6aaadc8bf78a030d810d3150133f5b083dc
nguyenkhackien <20020679@vnu.edu.vn>
2023-11-13 14:55:29 +0700

    sua lai thuat toan DCT toi uu hon

--- assets/src/DCT.py
+++ assets/src/DCT.py
@@ -1,8 +1,53 @@
 import cv2
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
-def dct(image):
+# dct block 8x8
+def dct(array):
+    result = np.zeros_like(array)
+    # dct row
+    for i in range(8):
+        for u in range(8):
+            sum=0
+            cu = 1/np.sqrt(2) if u==0 else 1
+            for v in range(8):
+                sum += array[i][v]*np.cos((2 * v + 1) * np.pi * u / 16)
+            result[i][u]=sum*cu*1/2
+    #dct col
+    for j in range(8):
+        for u in range(8):
+            sum=0
+            cu = 1/np.sqrt(2) if u==0 else 1
+            for v in range(8):
+                sum += array[v][j]*np.cos((2 * v + 1) * np.pi * u / 16)
+            result[u][j]=sum*cu*1/2
+    return result
+# idct block 8x8
+def idct(result):
+    reconstruction = np.zeros_like(result)
+    #idct row
+    for i in range(8):
+        for v in range(8):
+            sum=0
+            for u in range(8):
+                cu=1/np.sqrt(2) if u ==0 else 1
+                sum+=result[i][u]*cu*np.cos((2*v+1)*np.pi*u/16)
+            sum*=1/2
+            reconstruction[i][v]=sum
+    #idct col
+    for j in range(8):
+        for v in range(8):
+            sum=0
+            for u in range(8):
+                cu=1/np.sqrt(2) if u ==0 else 1
+                sum+=result[u][j]*cu*np.cos((2*v+1)*np.pi*u/16)
+            sum*=1/2
+            reconstruction[v][j]=sum
+    return reconstruction
+
+#dct function
+def dct_image(image):
     height, width = image.shape
     block_size = 8
     blocks_w = width + (block_size - width % block_size)
@@ -18,20 +63,12 @@ def dct(image):
 
     for i in range(0, blocks_h, block_size):
         for j in range(0, blocks_w, block_size):
-            for u in range(i, i + block_size):
-                for v in range(j, j + block_size):
-                    cu = 1 / np.sqrt(2) if u % 8 == 0 else 1
-                    cv = 1 / np.sqrt(2) if v % 8 == 0 else 1
-                    sum = 0
-                    for x in range(block_size):
-                        for y in range(block_size):
-                            sum += new_image[x + i, y + j] * np.cos((2 * x + 1) * (u % 8) * np.pi / 16) * np.cos(
-                                (2 * y + 1) * (v % 8) * np.pi / 16)
-                    result[u, v] = sum * cu * cv * (1 / 4)
-
+            block = new_image[i:i+block_size,j:j+block_size]
+            result[i:i+block_size,j:j+block_size]=dct(block)
     return result
 
-def idct(result):
+#idct function
+def idct_image(result):
     height, width = result.shape
     block_size = 8
 
@@ -39,16 +76,18 @@ def idct(result):
 
     for i in range(0, height, block_size):
         for j in range(0, width, block_size):
-            for x in range(i, i + block_size):
-                for y in range(j, j + block_size):
-                    sum = 0
-                    for u in range(block_size):
-                        for v in range(block_size):
-                            cu = 1 / np.sqrt(2) if u == 0 else 1
-                            cv = 1 / np.sqrt(2) if v == 0 else 1
-                            sum += result[u + i, v + j] * cu * cv * np.cos((2 * (x % 8) + 1) * u * np.pi / 16) * np.cos(
-                                (2 * (y % 8) + 1) * v * np.pi / 16)
-                    image[x, y] = sum * (1 / 4) + 128
-
+            block=result[i:i+block_size,j:j+block_size]
+            image[i:i+block_size,j:j+block_size]=idct(block)+128
     return image
 
+
+image = cv2.imread('../image/lena.jpg', cv2.IMREAD_GRAYSCALE)
+result = dct_image(image)
+reconstruction = idct_image(result)
+
+plt.gray()
+plt.subplot(121), plt.imshow(image), plt.axis('off'), plt.title('Original Image', size=10)
+
+# Hiển thị kết quả DCT trước khi lượng tử hóa
+plt.subplot(122), plt.imshow(reconstruction), plt.axis('off'), plt.title('DCT Result', size=10)
+plt.show()
