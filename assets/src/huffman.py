b9bb9b85f2439d59fefabf8f79ed2fcc4c5c0bbc
minhkien62 <20020170@vnu.edu.vn>
2023-11-22 21:49:44 +0700

    huffman fix zig zag reverse

--- assets/src/huffman.py
+++ assets/src/huffman.py
@@ -56,41 +56,53 @@ def decode_file(root, s):
             curr = root
     return ans
 
-##  Zig-zag
-def zig_zag(input_matrix):
-    z = np.empty([block_size*block_size])
-    index = -1
-    bound = 0
-    for i in range(0, 2 * block_size - 1):
-        if i < block_size:
-            bound = 0
-        else:
-            bound = i - block_size + 1
-        for j in range(bound, i - bound + 1):
-            index += 1
-            if i % 2 == 1:
-                z[index] = input_matrix[j, i-j]
-            else:
-                z[index] = input_matrix[i-j, j]
-    return z
 
-# def zig_zag_reverse(input_matrix):
-#     output_matrix = np.empty([block_size, block_size])
-#     index = -1
-#     bound = 0
-#     input_m = []
-#     for i in range(0, 2 * block_size - 1):
-#         if i < block_size:
-#             bound = 0
-#         else:
-#             bound = i - block_size + 1
-#         for j in range(bound, i - bound + 1):
-#             index += 1
-#             if i % 2 == 1:
-#                 output_matrix[j, i - j] = input_matrix[0][index]
-#             else:
-#                 output_matrix[i - j, j] = input_matrix[0][index]
-#     return output_matrix
+def zig_zag(matrix):
+    result = [0] * (block_size * block_size)
+    index = 0
+    
+    for i in range(block_size + block_size - 1):
+        if i % 2 == 0:  # Diagonal going up
+            row = min(i, block_size - 1)
+            col = max(0, i - block_size + 1)
+            while row >= 0 and col < block_size:
+                result[index] = matrix[row][col]
+                index += 1
+                row -= 1
+                col += 1
+        else:  # Diagonal going down
+            col = min(i, block_size - 1)
+            row = max(0, i - block_size + 1)
+            while col >= 0 and row < block_size:
+                result[index] = matrix[row][col]
+                index += 1
+                col -= 1
+                row += 1
+
+    return result
+
+def zig_zag_reverse(array):
+    result = [[0] * block_size for _ in range(block_size)]
+    index = 0
+    
+    for i in range(block_size + block_size - 1):
+        if i % 2 == 0:  # Diagonal going up
+            row = min(i, block_size - 1)
+            col = max(0, i - block_size + 1)
+            while row >= 0 and col < block_size:
+                result[row][col] = array[index]
+                index += 1
+                row -= 1
+                col += 1
+        else:  # Diagonal going down
+            col = min(i, block_size - 1)
+            row = max(0, i - block_size + 1)
+            while col >= 0 and row < block_size:
+                result[row][col] = array[index]
+                index += 1
+                col -= 1
+                row += 1
+    return result
 
 # Driver code
 if __name__ == "__main__":
@@ -175,7 +187,7 @@ if __name__ == "__main__":
     print(decodedStringDC)
     # print(zig_zag_reverse(decodedStringDC))
 
-    # print(zig_zag_reverse(decodedStringDC))
+    print(zig_zag_reverse(decodedStringDC))
     # Function call
     decodedStringRLC = decode_file(probsRLC[0], encodedStringRLC)
     print("\nDecoded Huffman Data:")
